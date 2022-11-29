b018097ff4896c5c32872202ac7cc22b7cf9cab6
Pedro Gabriel <pedro7109.j@gmail.com>
2022-11-29 12:02:45 -0300

    updating Backend

---
+++ Backend/src/config/server.js
@@ -0,0 +1,19 @@
+const port = 3000
+
+const bodyParse = require('body-parser');
+const express = require('express');
+const server = express();
+const allowCors = require('./cors')
+const queryParser = require('express-query-int')
+
+server.use(bodyParse.urlencoded({extended: true}))
+server.use(bodyParse.json())
+server.use(allowCors)
+server.use(queryParser())
+
+
+server.listen(port, function () {
+    console.log(`BACKEND is running on port ${port}.`)
+})
+
+module.exports = server
